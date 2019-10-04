db65db1d12b3a3ba13072791b4595629a8ea5876
Harry Mahardhika <harrymahardhika@gmail.com>
2019-10-04 16:44:33 +0700

    Fix gitignore path (2)

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ rm .php_cs.dist
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.php_cs.dist
 
 rm .editorconfig
-wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/editorconfig
-mv editorconfig .editorconfig
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.editorconfig
 
 rm .gitignore
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/gitignore
+mv gitignore .gitignore
 
 rm build_helper
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/build_helper
