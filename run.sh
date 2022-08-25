3a714fcf53acb919f72ea40189f789214f97835b
Harry Mahardhika <harrymahardhika@gmail.com>
2022-08-25 09:22:20 +0700

    fixes
    - use pint as code fixer
    - remove helper builder
    - add worker.log to .gitignore

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 rm .php_cs.dist
 rm .php_cs.dist.php
-wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.php_cs.dist.php
+# wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.php_cs.dist.php
 
 rm .editorconfig
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.editorconfig
@@ -20,8 +20,8 @@ wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/ma
 mv gitlab-ci.yml .gitlab-ci.yml
 
 rm build_helper
-wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/build_helper
-chmod +x build_helper
+# wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/build_helper
+# chmod +x build_helper
 
 rm phpstan.neon
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/phpstan.neon
@@ -30,13 +30,12 @@ rm php-cs-fixer
 
 chmod +x artisan
 
-composer require --dev friendsofphp/php-cs-fixer:^3.0
 composer require --dev barryvdh/laravel-debugbar
 composer require --dev nunomaduro/larastan
+composer require --dev laravel/pint
 
 tmp=$(mktemp)
-jq '.require .php = "^8.0"' composer.json > "$tmp" && mv "$tmp" composer.json
-jq '.scripts |= (.fix = "vendor/bin/php-cs-fixer fix --config=.php_cs.dist.php")' composer.json > "$tmp" && mv "$tmp" composer.json
 jq '.scripts |= (.phpstan = "vendor/bin/phpstan analyse")' composer.json > "$tmp" && mv "$tmp" composer.json
+jq '.scripts |= (.pint = "vendor/bin/pint")' composer.json > "$tmp" && mv "$tmp" composer.json
 
-composer run fix
+composer run pint
