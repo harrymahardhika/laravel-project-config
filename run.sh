44c62a78aa4f7950902063cbb41a322120a64051
Harry Mahardhika <harrymahardhika@gmail.com>
2022-09-10 21:04:59 +0700

    updates
    - add pint
    - add prettier
    - add bladeformatter
    - fix filenames

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 rm .php_cs.dist
 rm .php_cs.dist.php
-# wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.php_cs.dist.php
+rm php-cs-fixer
 
 rm .editorconfig
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.editorconfig
@@ -12,30 +12,39 @@ wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/ma
 mv gitignore .gitignore
 
 rm .env.gitlab-ci
-wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/env.gitlab-ci
-mv env.gitlab-ci .env.gitlab-ci
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.env.gitlab-ci
 
 rm .gitlab-ci.yml
-wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/gitlab-ci.yml
-mv gitlab-ci.yml .gitlab-ci.yml
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.gitlab-ci.yml
 
 rm build_helper
-# wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/build_helper
-# chmod +x build_helper
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/build_helper
+chmod +x build_helper
 
 rm phpstan.neon
 wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/phpstan.neon
 
-rm php-cs-fixer
+rm pint.json
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/pint.json
+
+rm .bladeformatterrc.json
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.bladeformatterrc.json
+
+rm .prettierrc.json
+wget https://raw.githubusercontent.com/harrymahardhika/laravel-project-config/master/.prettierrc.json
 
 chmod +x artisan
 
-composer require --dev barryvdh/laravel-debugbar
-composer require --dev nunomaduro/larastan
-composer require --dev laravel/pint
+composer require --dev barryvdh/laravel-debugbar barryvdh/laravel-ide-helper nunomaduro/larastan laravel/pint
+
+npm install --save-dev blade-formatter prettier prettier-plugin-organize-attributes
+npm uninstall lodash postcss
 
 tmp=$(mktemp)
 jq '.scripts |= (.phpstan = "vendor/bin/phpstan analyse")' composer.json > "$tmp" && mv "$tmp" composer.json
-jq '.scripts |= (.pint = "vendor/bin/pint")' composer.json > "$tmp" && mv "$tmp" composer.json
+jq '.scripts |= (.format = [])' composer.json > "$tmp" && mv "$tmp" composer.json
+jq '.scripts.format |= .+["vendor/bin/pint --verbose"]' composer.json > "$tmp" && mv "$tmp" composer.json
+jq '.scripts.format |= .+["node_modules/.bin/prettier -w -l resources/js"]' composer.json > "$tmp" && mv "$tmp" composer.json
+jq --indent 4 '.scripts.format |= .+["node_modules/.bin/blade-formatter -w -d resources/**/*.blade.php"]' composer.json > "$tmp" && mv "$tmp" composer.json
 
-composer run pint
+composer run format
